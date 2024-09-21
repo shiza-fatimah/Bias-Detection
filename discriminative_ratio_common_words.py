e41091730ffa3b0f1297be0c35080a85aaa7d28d
Shiza Fatimah <fatimahs0@>
2024-09-21 16:23:16 +0200

    remove commented code

--- discriminative_ratio_common_words.py
+++ discriminative_ratio_common_words.py
@@ -1,73 +1,80 @@
 import re
 import string
 import nltk
-from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
 from nltk import pos_tag
 from collections import Counter
 import pandas as pd
-from collections import Counter
 import os
 from loguru import logger
-from data_preprocessing import Preprocessor
+import contractions
 
-#nltk.download('averaged_perceptron_tagger')
-#nltk.download('punkt')
-#nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('words')
 
-def preprocess(text):
-    #text = text.replace("NEW_PARAGRAPH", "")
-    #text = text.lower()
-    #text = re.sub(r'\d+', '', text)
-    #text = text.translate(str.maketrans('', '', string.punctuation))
+lemmatizer = WordNetLemmatizer()
+
+def preprocess(text,freq_threshold, word_freq):
+    
+    text = contractions.fix(text)
+    text = text.lower()
+    text = re.sub(r'\d+', '', text)
+    text = text.translate(str.maketrans('', '', string.punctuation))
     words = word_tokenize(text)
-    #stop_words = set(stopwords.words('english'))
-    #words = [word for word in words if word not in stop_words]
-    tagged_words = pos_tag(words)
-    # Filter for content words and remove stop words
-    content_words = [word for word, tag in tagged_words if tag.startswith(('N', 'V', 'J', 'R'))]
-    return content_words
+    stop_words = set(stopwords.words('english'))
+    words = [word for word in words if word not in stop_words]
+    # Lemmatize the words
+    words = [lemmatizer.lemmatize(word) for word in words]
+     # Filter out words that are less frequent than the threshold
+    words = [word for word in words if word_freq[word] >= freq_threshold]
+    # Remove single letters and non-alphabetic words
+    words = [word for word in words if len(word) > 1 and word.isalpha()]
     
-
-def save_ratio_as_csv(df_discriminative_ratio):
-    file_name = f"discriminative_ratio_{target}_{comparison}.csv"
-    current_directory = os.path.dirname(__file__)
-    file_path = os.path.join(current_directory, file_name)
-    df_discriminative_ratio.to_csv(file_path, index=False)
-    logger.info(f"DataFrame saved to: {file_path}")
-    
+    return words
+        
 
 # Load the dataset
-df = pd.read_csv('allsides-df.csv')
-
-#preprocess
-preprocessor = Preprocessor(df)
-df = preprocessor.preprocess()
+df = pd.read_csv('allsides-df-processed.csv')
+df = df.dropna(subset=['content'])
+df = df.dropna(subset=['bias'])
 
 # Specify the columns you want to keep
-columns_to_keep = ['title', 'story_group', 'time', 'author', 'bias', 'source', 'url', 'topic', 'content']
+columns_to_keep = ['bias', 'content']
 
 # Drop the rest of the columns, keeping only the specified ones
 filtered_df = df[columns_to_keep]
 
+# Combine all texts to calculate word frequencies
+all_texts = filtered_df['content'].tolist()
+all_words = [word for text in all_texts for word in preprocess(text, 0, Counter())] 
+word_freq = Counter(all_words)
+
+# Set the frequency threshold
+freq_threshold = 10
+
 # Filter
-final_df = filtered_df[filtered_df['bias'].isin(['Left', 'Right', 'Center'])]
+filtered_df = filtered_df[filtered_df['bias'].isin(['Left', 'Right', 'Center'])]
 
 # 'target' represents the frequency in the target text type (numerator), and
 # 'comparison' represents the frequency in the comparison text type (denominator)
 
 target = 'Left'
-comparison = 'Center'
+comparison = 'Right'
 
 # Sample data preparation
-texts_target = final_df[final_df['bias'] == target]['content'].tolist()
-texts_comparison = final_df[final_df['bias'] == comparison]['content'].tolist()
+texts_target = filtered_df[filtered_df['bias'] == target]['content'].tolist()
+texts_comparison = filtered_df[filtered_df['bias'] == comparison]['content'].tolist()
 
 
 # Preprocess the texts
 logger.info("Starting preprocessing for text.")
-texts_target = [preprocess(text) for text in texts_target]
-texts_comparison = [preprocess(text) for text in texts_comparison]
+texts_target = [preprocess(text, freq_threshold, word_freq) for text in texts_target]
+texts_comparison = [preprocess(text, freq_threshold, word_freq) for text in texts_comparison]
 logger.info("Finished preprocessing for text.")
 
 
@@ -101,4 +108,12 @@ sorted_discriminativeness_ratio = sorted(discriminativeness_ratio.items(), key=l
 
 df_discriminative_ratio = pd.DataFrame(sorted_discriminativeness_ratio, columns=['word', 'ratio'])
 
-save_ratio_as_csv(df_discriminative_ratio)
+# Print the first 5 results
+print("Top 5 Discriminativeness Ratios of common words:")
+for word, ratio in sorted_discriminativeness_ratio[:5]:
+    print(f"Word: {word}, Discriminativeness Ratio: {ratio:.4f}")
+
+# Print the last 5 results
+print("\nBottom 5 Discriminativeness Ratios of common words:")
+for word, ratio in sorted_discriminativeness_ratio[-5:]:
+    print(f"Word: {word}, Discriminativeness Ratio: {ratio:.4f}")
